5b9e20141a9e166e06605bad8ff20ec88bf012a1
İpek Özbay <ipek.ozbay@bil.omu.edu.tr>
2021-05-28 23:03:12 +0300

    Delete Hrms/Hrms directory

--- Hrms/Hrms/src/main/java/kodlama/io/Hrms/business/abstracts/JobPositionService.java
+++
@@ -1,9 +0,0 @@
-package kodlama.io.Hrms.business.abstracts;
-
-import java.util.*;
-
-import kodlama.io.Hrms.entities.concrete.JobPosition;
-
-public interface JobPositionService {
-	List<JobPosition> getAll();
-}
--- Hrms/Hrms/src/main/java/kodlama/io/Hrms/dataAccess/abstracts/JobPositionDao.java
+++
@@ -1,9 +0,0 @@
-package kodlama.io.Hrms.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kodlama.io.Hrms.entities.concrete.JobPosition;
-
-public interface JobPositionDao extends JpaRepository<JobPosition ,Integer>{
-
-}
