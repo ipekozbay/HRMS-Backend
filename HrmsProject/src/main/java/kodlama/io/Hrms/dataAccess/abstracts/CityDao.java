226bf2e519812e6144f75d4cdb164a26ee6da4e9
İpek Özbay <ipek.ozbay@bil.omu.edu.tr>
2021-06-09 20:00:10 +0300

    dataAccess layer completed

--- HrmsProject/src/main/java/kodlama/io/Hrms/dataAccess/abstracts/CityDao.java
+++ HrmsProject/src/main/java/kodlama/io/Hrms/dataAccess/abstracts/CityDao.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import kodlama.io.Hrms.entities.concrete.City;
 
 public interface CityDao extends JpaRepository<City, Integer> {
-	City getById(int id);
+	City getById(int id); 
 }
